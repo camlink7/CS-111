7a49264cc80caab8e4ff5ff0385b6510fad70b64
Camden <camdenlink7@gmail.com>
2023-04-05 10:02:35 -0400

    began working on the Simulator class

--- Process.java
+++ Process.java
@@ -1,11 +1,34 @@
+/*
+ * Name: Camden Link
+ * Assignment: Program 3
+ * Lab Instructor: Jakob Kaivo
+ * Date: 2023-04-05
+ * Purpose: This file contains the Process class, representing a CPU process as specified in Program 3
+ */
+
+/*
+ * Class Name: Process
+ * Purpose: Class that allows for the calculation of information about the Process as well as executing the process
+ * Exceptions: N/A
+ */
 public class Process {
-	private final int TIME_SLICE_LENGTH = 100; //In Milliseconds
+	private static final int TIME_SLICE_LENGTH = 100; //In Milliseconds
 	
 	private String id;
 	private int burstTime; //In Milliseconds
 	private int totalWaitTime; //In Milliseconds
 	private int lastExecutionTime; //In Milliseconds
 	
+	/*
+	 * Method Name: Process
+	 * Purpose: Constructs a new Rectangle object
+	 * Parameters:
+	 * - id: The identifier for the process
+	 * - burstTime: The total amount of time it takes for the execution to complete
+	 * Preconditions: N/A
+	 * Postconditions: N/A
+	 * Exceptions: N/A
+	 */
 	public Process(String id, int burstTime) {
 		this.id = id;
 		this.burstTime = burstTime * 1000;
@@ -20,6 +43,7 @@ public class Process {
 	public int getBurst() {
 		return this.burstTime;
 	}
+	
 
 	public int getWaitTime() {
 		return this.totalWaitTime;
--- Simulator.java
+++ Simulator.java
@@ -1,11 +1,21 @@
 import java.util.Scanner;
 
 public class Simulator {
+	public static final int MS_PER_MINUTE = 60000;
+	public static final int MS_PER_SECOND = 1000;
+
+	
 	public static PriorityQueue enqueueProcesses(String filename, boolean priority) {
 		return null;
 	}
 
 	public static void simulate(PriorityQueue queue) {
+		int currentTime = 0; //In Milliseconds
+		while (!queue.isEmpty()) {
+			Process process = queue.dequeue();
+			process.execute(currentTime);
+			System.out.println(process.getID() + " excuted; " + Simulator.formatTimestamp(process.getBurst()) + " remaining");
+		}
 	}
 
 	public static void main(String[] args) {
@@ -19,5 +29,11 @@ public class Simulator {
 
 		PriorityQueue priority = enqueueProcesses(filename, true);
 		simulate(priority);
+	}
+	
+	public static String formatTimestamp(int milliseconds) {
+		int minutes = milliseconds / MS_PER_MINUTE;
+		int seconds = milliseconds / MS_PER_SECOND;
+		return "";
 	}
 }
